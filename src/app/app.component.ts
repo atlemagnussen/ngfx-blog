a2018b37897126e8e2d324a112b903c557311009
Atle Magnussen <atlemagnussen@gmail.com>
2019-08-29 14:15:48 +0200

    initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from "@angular/core";
+
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+
+import { Post } from '../models/post.model';
+import * as PostActions from './post.actions';
+
+interface AppState {
+    post: Post
+};
+
+@Component({
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"],
+})
+export class AppComponent {
+    
+    post: Observable<Post>;
+    text: string;
+
+    constructor(private store: Store<AppState>) {
+        this.post = this.store.select('post');
+    }
+
+    editText() {
+        this.store.dispatch(new PostActions.EditText(this.text));
+    }
+
+    resetPost() {
+        this.store.dispatch(new PostActions.Reset());
+    }
+
+    upvote() {
+        this.store.dispatch(new PostActions.Upvote());
+    }
+
+    downvote() {
+        this.store.dispatch(new PostActions.Downvote());
+    }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from '@angular/forms';
+
+import { StoreModule } from "@ngrx/store";
+import { postReducer } from "./post.reducer";
+
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+      BrowserModule,
+      FormsModule,
+      StoreModule.forRoot({
+          post: postReducer,
+      })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
